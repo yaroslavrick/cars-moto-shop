eb0f827ea1750bcd88867f24d5850b69a811208d
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-04 16:19:55 +0200

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+25.times do
+  Car.create(
+    make: FFaker::Vehicle.make,
+    model: FFaker::Vehicle.model,
+    year: FFaker::Vehicle.year,
+    odometer: FFaker::Random.rand(1..300_000),
+    price: FFaker::Random.rand(1000..500_00)
+  )
+end
