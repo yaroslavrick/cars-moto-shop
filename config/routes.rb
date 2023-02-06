3ab8783c0a78df1fe6f5ea2a2013c40e4bab835a
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-06 17:40:30 +0200

    Added HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'home/index'
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     resources :cars
+    root 'home#index'
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
     # Defines the root path route ("/")
