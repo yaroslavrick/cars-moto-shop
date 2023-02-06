f9271ef963f1b5e24352eaeaefeabdeff398c5d4
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-07 00:31:12 +0200

    Added sorting in Cars

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'home/index'
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
-    resources :cars
     root 'home#index'
+    resources :cars
+    get 'help/index'
+    get 'home/index'
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
     # Defines the root path route ("/")
