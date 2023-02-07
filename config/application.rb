a4d02cd449e177d82cced057dc89108eabc62fdb
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-07 10:51:53 +0200

    fix in translations

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module CarAdvertisementsWeb
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.i18n.available_locales = %i[en ua]
+    config.i18n.available_locales = %i[en uk]
     config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
