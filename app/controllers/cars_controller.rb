1514a0dca3261ad5ef65dd690a25d0eaebd6db14
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-08 14:47:51 +0200

    Cleared CarsController from commented code

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,78 +3,16 @@
 class CarsController < ApplicationController
   # before_action :set_car, only: %i[show edit update destroy]
 
-  SORTING = {
-    'Date added asc' => { created_at: :asc },
-    'Date added desc' => { created_at: :desc },
-    'Price asc' => { price: :asc },
-    'Price desc' => { price: :desc }
-  }.freeze
-
-  # GET /cars or /cars.json
   def index
-    # binding.pry
     @cars = SearchEngine.new(params: car_search_params).data
-    # @cars = @cars.order(**SORTING[params[:sort_by]]) if params[:sort_by].present?
     @pagy, @cars = pagy @cars
   end
 
-  # GET /cars/1 or /cars/1.json
   def show
     @car = Car.find(params[:id])
   end
 
-  # GET /cars/new
-  # def new
-  #   @car = Car.new
-  # end
-
-  # GET /cars/1/edit
-  # def edit; end
-
-  # POST /cars or /cars.json
-  # def create
-  #   @car = Car.new(car_params)
-  #
-  #   respond_to do |format|
-  #     if @car.save
-  #       format.html { redirect_to car_url(@car), notice: I18n.t('cars_controller.car_was_successfully_created') }
-  #       format.json { render :show, status: :created, location: @car }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @car.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # PATCH/PUT /cars/1 or /cars/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @car.update(car_params)
-  #       format.html { redirect_to car_url(@car), notice: I18n.t('cars_controller.car_was_successfully_updated') }
-  #       format.json { render :show, status: :ok, location: @car }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @car.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # DELETE /cars/1 or /cars/1.json
-  # def destroy
-  #   @car.destroy
-  #
-  #   respond_to do |format|
-  #     format.html { redirect_to cars_url, notice: I18n.t('cars_controller.car_was_successfully_destroyed') }
-  #     format.json { head :no_content }
-  #   end
-  # end
-
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_car
-  #   @car = Car.find(params[:id])
-  # end
 
   # Only allow a list of trusted parameters through.
   # def car_params
@@ -87,6 +25,5 @@ class CarsController < ApplicationController
 
   def search_car
     @car = Lib::SearchEngine.new(params: car_search_params)
-    # Car.all
   end
 end
