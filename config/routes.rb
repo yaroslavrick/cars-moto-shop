cf263a786c177a3a188ec539261452a519a18d3d
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-08 14:38:57 +0200

    WIP on search

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     root 'home#index'
-    resources :cars
-    get 'help/index'
-    get 'home/index'
+    resources :cars, only: %i[index show]
+    resources :help, only: %i[index]
+    resources :home, only: %i[index]
+    resources :search, only: %i[index]
+    # get 'help/index'
+    # get 'home/index'
+    # get 'search/index'
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
     # Defines the root path route ("/")
