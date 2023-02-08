db0b16cd3d4e255b44caf332a832d68b6da3b4d8
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-08 16:19:15 +0200

    Created separate service SortService

---
+++ app/services/cars_service/sort_engine.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module CarsService
+  class SortEngine
+    SORTING = {
+      'Date added asc' => { created_at: :asc },
+      'Date added desc' => { created_at: :desc },
+      'Price asc' => { price: :asc },
+      'Price desc' => { price: :desc }
+    }.freeze
+
+    attr_reader :params, :data
+
+    def initialize(params:, data:)
+      @params = params
+      @data = data
+      call
+    end
+
+    def call
+      @data = sort
+    end
+
+    private
+
+    def sort
+      return data if params[:sort_by].blank?
+
+      data.order(**SORTING[params[:sort_by]]) if params[:sort_by].present?
+    end
+  end
+end
