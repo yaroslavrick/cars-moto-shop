4960a2daed5de5f45fa4431e3aac9d6108d48092
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-09 14:02:23 +0200

    Added Login controller, del comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'login/index'
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     root 'home#index'
     resources :cars, only: %i[index show]
     resources :help, only: %i[index]
     resources :home, only: %i[index]
     resources :search, only: %i[index]
-    # get 'help/index'
-    # get 'home/index'
-    # get 'search/index'
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-    # Defines the root path route ("/")
-    # root "articles#index"
   end
 end
