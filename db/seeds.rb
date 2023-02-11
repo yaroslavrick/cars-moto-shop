b5d27fd58125049d490d888f6a7a281863f8835e
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-11 08:09:31 +0200

    Refactored seeds, moved car creation to seeds/car

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,3 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-25.times do
-  Car.create(
-    make: FFaker::Vehicle.make,
-    model: FFaker::Vehicle.model,
-    year: Time.zone.at(Time.new(1990).to_f + (rand * (Time.now.to_f - Time.new(1990).to_f))).to_date.year,
-    odometer: FFaker::Random.rand(1..300_000),
-    price: FFaker::Random.rand(1000..500_00),
-    description: FFaker::Book.description
-  )
-end
+require_relative 'seeds/car'
---
+++ db/seeds/car.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+def create_car
+  Car.create(
+    make: FFaker::Vehicle.make,
+    model: FFaker::Vehicle.model,
+    year: generate_year,
+    odometer: FFaker::Random.rand(1..300_000),
+    price: FFaker::Random.rand(1000..500_00),
+    description: FFaker::Book.description
+  )
+end
+
+def generate_year
+  Time.zone.at(Time.new(1990).to_f + (rand * (Time.now.to_f - Time.new(1990).to_f))).to_date.year
+end
+
+25.times { create_car }
