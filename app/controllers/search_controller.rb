3df0f8fe27b601d1b0693c42c853172c044f9448
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-11 18:30:48 +0200

    Fixed form: my searches is working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,6 +12,7 @@ class SearchController < ApplicationController
 
   def create
     @search = Search.create(search_params.merge(user_id: current_user.id)) if current_user
+    redirect_to cars_path(filter_params)
   end
 
   private
@@ -19,7 +20,10 @@ class SearchController < ApplicationController
   def search_params
     return false if params['filter_params'].blank?
 
-    # params['filter_params'].permit(:make, :model, :year_from, :year_to, :price_from, :price_to)
     params.require(:filter_params).permit(:make, :model, :year_from, :year_to, :price_from, :price_to)
+  end
+
+  def filter_params
+    params.permit(filter_params: {})
   end
 end
