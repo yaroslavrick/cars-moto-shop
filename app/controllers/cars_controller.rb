5d6f70b5cbfaa0d20fb81ee9d3860b616e7d7ace
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-11 18:30:48 +0200

    Fixed form: my searches is working

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class CarsController < ApplicationController
-  # before_action :set_car, only: %i[show edit update destroy]
-
   def index
     @cars = Car.all
     @cars = CarsService::SearchEngine.new(params: params['filter_params'], data: @cars).call if search_params.present?
@@ -21,7 +19,6 @@ class CarsController < ApplicationController
   def search_params
     return false if params['filter_params'].blank?
 
-    # params['filter_params'].permit(:make, :model, :year_from, :year_to, :price_from, :price_to)
     params.require(:filter_params).permit(:make, :model, :year_from, :year_to, :price_from, :price_to)
   end
 
