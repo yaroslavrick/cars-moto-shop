5a934755733860ae80020188fc9959b73e5bede1
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-15 23:31:03 +0200

    Switch validates_associated to search

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,10 +3,5 @@
 class Search < ApplicationRecord
   belongs_to :user
 
-  # validates :make, allow_nil: true
-  # validates :model
-  # validates :year_from, numericality: { only_integer: true, greater_than: 1900 }, allow_nil: true
-  # validates :year_to, numericality: { only_integer: true, in: 1900..Time.zone.today.year }
-  # validates :price_from, numericality: true, allow_nil: true
-  # validates :price_to, numericality: true, allow_nil: true
+  validates_associated :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates_associated :searches
 end
