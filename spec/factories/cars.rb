0e06de7f4058f3ec40c3769ab851c0d645fc067c
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-15 20:38:36 +0200

    Added tests to CarsModels

--- spec/factories/cars.rb
+++ spec/factories/cars.rb
@@ -4,12 +4,9 @@ FactoryBot.define do
   factory :car do
     make { FFaker::Vehicle.make }
     model { FFaker::Vehicle.model }
-    year { generate_year }
+    year { Time.zone.at(Time.new(1990).to_f + (rand * (Time.now.to_f - Time.new(1990).to_f))).to_date.year }
     odometer { FFaker::Random.rand(1..300_000) }
     price { FFaker::Random.rand(1000..500_00) }
-    description { FFaker::Book.description }
-  end
-  def generate_year
-    Time.zone.at(Time.new(1990).to_f + (rand * (Time.now.to_f - Time.new(1990).to_f))).to_date.year
+    description { FFaker::Lorem.characters(300) }
   end
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -3,5 +3,120 @@
 require 'rails_helper'
 
 RSpec.describe Car do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:car) { build(:car) }
+
+  # subject(described_class.new)
+
+  context 'when presence validation' do
+    it 'is valid with valid attributes' do
+      expect(car).to be_valid
+    end
+
+    it 'is not valid without a make' do
+      car.make = nil
+      expect(car).not_to be_valid
+    end
+
+    it 'is not valid without a model' do
+      car.model = nil
+      expect(car).not_to be_valid
+    end
+
+    it 'is not valid without a year' do
+      car.year = nil
+      expect(car).not_to be_valid
+    end
+
+    it 'is not valid without a odometer' do
+      car.odometer = nil
+      expect(car).not_to be_valid
+    end
+
+    it 'is not valid without a price' do
+      car.price = nil
+      expect(car).not_to be_valid
+    end
+
+    it 'is not valid without a description' do
+      car.description = nil
+      expect(car).not_to be_valid
+    end
+  end
+
+  context 'when validation types' do
+    it 'not valid make length with non valid length' do
+      car.make = 'A'
+      expect(car).not_to be_valid
+    end
+
+    it 'validate make length with valid length' do
+      car.make = 'Ab'
+      expect(car).to be_valid
+    end
+
+    it 'not valid with year non integer value' do
+      car.year = ''
+      expect(car).not_to be_valid
+    end
+
+    it 'validate year numericality with valid value' do
+      car.year = 1951
+      expect(car).to be_valid
+    end
+
+    it 'validate year greater than 1900' do
+      car.year = 1901
+      expect(car).to be_valid
+    end
+
+    it 'not valid with year less than 1900' do
+      car.year = 1899
+      expect(car).not_to be_valid
+    end
+
+    it 'not valid odometer numericality with non integer value' do
+      car.odometer = ''
+      expect(car).not_to be_valid
+    end
+
+    it 'validate odometer numericality' do
+      car.odometer = 80_000
+      expect(car).to be_valid
+    end
+
+    it 'not validate price with non numeric value' do
+      car.price = ''
+      expect(car).not_to be_valid
+    end
+
+    it 'validate price numericality' do
+      car.price = 9.99
+      expect(car).to be_valid
+    end
+
+    it 'not validate price equal 0' do
+      car.price = 0
+      expect(car).not_to be_valid
+    end
+
+    it 'validate price greater than 0' do
+      car.price = 0.5
+      expect(car).to be_valid
+    end
+
+    it 'not validate description with length less than 6 symbols' do
+      car.description = '12345'
+      expect(car).not_to be_valid
+    end
+
+    it 'not validate description with length more than 300 symbols' do
+      car.description = FFaker::Lorem.characters(301)
+      expect(car).not_to be_valid
+    end
+
+    it 'validate description length' do
+      car.description = FFaker::Lorem.characters(300)
+      expect(car).to be_valid
+    end
+  end
 end
