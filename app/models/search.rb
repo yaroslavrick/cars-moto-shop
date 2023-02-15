6416ff305547c5f4d39ceaf9991aea7b7cc84336
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-15 16:26:02 +0200

    Added validations to models

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,6 +2,12 @@
 
 class Search < ApplicationRecord
   belongs_to :user
-  # validates :year_from, numerically: true
-  # validates :year_to, numerically: true
+
+  validates :make, presence: true,
+                   length: { minimum: 2, too_short: '%<count>s characters is the minimum allowed' },
+                   allow_blank: true
+  validates :model, presence: true, allow_blank: true
+  validates :year, presence: true, numericality: { only_integer: true, greater_than: 1900 }, allow_nil: true
+  validates :odometer, presence: true, numericality: { only_integer: true }, allow_nil: true
+  validates :price, presence: true, numericality: true, allow_nil: true
 end
