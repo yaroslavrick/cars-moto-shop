664ec7c4cf3b511dcf37b97f12544a5a28fa8629
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-18 21:59:01 +0200

    Refactored code in routes, _car_card, views_specs

--- spec/views/searches/index.html.erb_spec.rb
+++ spec/views/searches/index.html.erb_spec.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+RSpec.describe 'search/index.html.erb' do
+  context 'when user is not logged in' do
+    it 'redirects to authentication page'
+  end
+
+  context 'when user in logged in' do
+    it 'show user searches'
+  end
+end
