17f099b765565a03cb2bb7a6520791b098f2b12d
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-18 22:34:40 +0200

    Refactored routes.rb, rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,7 @@ require 'shields_badge'
 SimpleCov.formatter = SimpleCov::Formatter::ShieldsBadge
 # SimpleCov.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'devise'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -79,4 +80,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 end
--- spec/views/searches/index.html.erb_spec.rb
+++ spec/views/searches/index.html.erb_spec.rb
@@ -1,13 +1,33 @@
 # frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe 'search/index.html.erb' do
-  context 'when user is not logged in' do
-    it 'redirects to authentication page'
-  end
-
-  context 'when user in logged in' do
-    it 'show user searches'
-  end
-end
+# # frozen_string_literal: true
+#
+# require 'rails_helper'
+#
+# RSpec.describe 'searches/index.html.erb' do
+#   let(:user) { create(:user) }
+#
+#   context "when user is authenticated" do
+#     before do
+#       sign_in user
+#       assign(:user_searches, [])
+#       assign(:pagy, double('pagy', page: 1))
+#     end
+#
+#     it "renders successfully" do
+#       render
+#       expect(rendered).to match /Searches/
+#     end
+#   end
+#
+#   context "when user is not authenticated" do
+#     before do
+#       assign(:user_searches, [])
+#       assign(:pagy, double('pagy', page: 1))
+#     end
+#
+#     it "redirects to login page" do
+#       render
+#       expect(rendered).to have_selector("form[action='#{new_user_session_path}']")
+#     end
+#   end
+# end
