6891696332569605b62e14b39d8dea433f7ed09e
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-21 00:49:57 +0200

    Added tests to SearchesController

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe SearchesController do
   let(:user) { create(:user) }
+  let(:search) { create(:search, :search_zaz_sens) }
 
   describe 'GET #index' do
     context 'when user logged' do
@@ -25,6 +26,22 @@ RSpec.describe SearchesController do
 
       it 'redirects to sign_up page' do
         expect(response).to redirect_to(new_user_session_path(locale: I18n.default_locale))
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    context 'when user is logged in' do
+      it 'creates new search' do
+        expect do
+          post :create,
+               params: { filter_params: search }
+        end.to change(Search, :count).by(1)
+      end
+
+      it 'redirects to cars_path' do
+        post :create, params: { filter_params: search }
+        expect(response).to redirect_to(cars_path)
       end
     end
   end
