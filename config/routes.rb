132686b4fff6aa9fd78919d4822f37d08e411a14
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-21 15:33:11 +0200

    Moved Cars routes to scope(:locale)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :cars, only: %i[index show]
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users
     root 'homes#index'
+    resources :cars, only: %i[index show]
     resources :helps, only: %i[index]
     resources :homes, only: %i[index]
     resources :searches, only: %i[index new create]
