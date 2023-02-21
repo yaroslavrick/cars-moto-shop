3d77e0f4e18895c669f14d581e8654b3c0473927
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-21 12:53:15 +0200

    Del redundant Module, added new_line at the end of some files

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
-require_relative './controller_macros'
-
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Warden::Test::Helpers
   config.include Devise::Test::IntegrationHelpers, type: :feature
-  config.extend ControllerMacros, type: :controller
 end
