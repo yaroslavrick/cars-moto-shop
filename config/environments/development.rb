882fae9f9f1a40924da1118f2b76e53226b4246b
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-21 14:17:55 +0200

    Added host name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,13 +9,11 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.hosts << 'caradvertisementsweb-production.up.railway.app'
   # Do not eager load code on boot.
   config.eager_load = false
-
   # Show full error reports.
   config.consider_all_requests_local = true
-
   # Enable server timing
   config.server_timing = true
 
