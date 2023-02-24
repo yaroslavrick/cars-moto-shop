984ee840f733cef25f74e9924e6fad17626b7886
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-24 16:56:36 +0200

    Added validation to sort_params

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class CarsController < ApplicationController
+  SORT_PARAMS = ['created_at asc', 'created_at desc', 'price asc', 'price desc'].freeze
+
   def index
     @cars = Car.all
     @cars = CarsService::SearchService.new(params: params['filter_params'], data: @cars).call if search_params.present?
-    @cars = CarsService::SortService.new(params: params[:sort_by], data: @cars).call if sort_params.present?
+    @cars = CarsService::SortService.new(params: params[:sort_by], data: @cars).call if valid_sort_params?
     @total_cars_count = @cars.count
     @pagy, @cars = pagy @cars
   end
@@ -21,7 +23,10 @@ class CarsController < ApplicationController
     params.require(:filter_params).permit(:make, :model, :year_from, :year_to, :price_from, :price_to)
   end
 
-  def sort_params
-    params.permit(:sort_by)
+  def valid_sort_params?
+    return false if params[:sort_by].blank?
+
+    sort_params = params[:sort_by].downcase
+    SORT_PARAMS.include?(sort_params)
   end
 end
