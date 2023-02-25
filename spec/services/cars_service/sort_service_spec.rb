c3aa4ba790357b1926c4de9aaedb25e11932c12f
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-25 14:49:42 +0200

    Added proper describe name to service spec

--- spec/services/cars_service/sort_service_spec.rb
+++ spec/services/cars_service/sort_service_spec.rb
@@ -3,41 +3,43 @@
 require 'rails_helper'
 
 RSpec.describe CarsService::SortService, type: :service do
-  subject(:cars) { described_class.new(params:, data: Car.all).call }
+  describe '#call' do
+    subject(:cars) { described_class.new(params:, data: Car.all).call }
 
-  let(:car1) { create(:car, :zaz_sens) }
-  let(:car2) { create(:car, :opel_kadett) }
-  let(:car3) { create(:car, :nissan_leaf) }
+    let(:car1) { create(:car, :zaz_sens) }
+    let(:car2) { create(:car, :opel_kadett) }
+    let(:car3) { create(:car, :nissan_leaf) }
 
-  context 'when sorting by created_at ASC' do
-    let(:params) { { 'created_at' => 'ASC' } }
+    context 'when sorting by created_at ASC' do
+      let(:params) { { 'created_at' => 'ASC' } }
 
-    it 'returns sorted data' do
-      expect(cars).to eq([car1, car2, car3])
+      it 'returns sorted data' do
+        expect(cars).to eq([car1, car2, car3])
+      end
     end
-  end
 
-  context 'when sorting by created_at DESC' do
-    let(:params) { { 'created_at' => 'DESC' } }
+    context 'when sorting by created_at DESC' do
+      let(:params) { { 'created_at' => 'DESC' } }
 
-    it 'returns sorted data' do
-      expect(cars).to eq([car1, car2, car3].reverse)
+      it 'returns sorted data' do
+        expect(cars).to eq([car1, car2, car3].reverse)
+      end
     end
-  end
 
-  context 'when sorting by price ASC' do
-    let(:params) { { 'price' => 'ASC' } }
+    context 'when sorting by price ASC' do
+      let(:params) { { 'price' => 'ASC' } }
 
-    it 'returns sorted data' do
-      expect(cars).to eq([car2, car1, car3])
+      it 'returns sorted data' do
+        expect(cars).to eq([car2, car1, car3])
+      end
     end
-  end
 
-  context 'when sorting by price DESC' do
-    let(:params) { { 'price' => 'DESC' } }
+    context 'when sorting by price DESC' do
+      let(:params) { { 'price' => 'DESC' } }
 
-    it 'returns sorted data' do
-      expect(cars).to eq([car2, car1, car3].reverse)
+      it 'returns sorted data' do
+        expect(cars).to eq([car2, car1, car3].reverse)
+      end
     end
   end
 end
