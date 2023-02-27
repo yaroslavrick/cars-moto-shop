b5105ccac281fc34c35f80c6799c4f90dc8d804c
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-27 16:39:16 +0200

    Added validations to user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@
 
 class User < ApplicationRecord
   has_many :searches, dependent: :delete_all
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { within: 8..128 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
