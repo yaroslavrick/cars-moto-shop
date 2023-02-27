0d91e30b9db3067eeb880a56eb1eea97a7b67a47
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-27 19:09:11 +0200

    Added SimpleCov::Formatter::ShieldsBadge to the group

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,8 @@ SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 # SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::LcovFormatter,
-  SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::ShieldsBadge
 ])
 SimpleCov.start 'rails' do
   minimum_coverage 70
@@ -14,7 +15,7 @@ SimpleCov.start 'rails' do
 end
 
 require 'shields_badge'
-SimpleCov.formatter = SimpleCov::Formatter::ShieldsBadge
+# SimpleCov.formatter = SimpleCov::Formatter::ShieldsBadge
 # SimpleCov.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
