b84c5e1530e567be056d54d69d8b92bfd0b2f657
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-27 15:41:07 +0200

    Validate associated

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Search do
 
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
+    it { is_expected.to validate_presence_of(:user).with_message('must exist') }
   end
 
   describe 'columns' do
