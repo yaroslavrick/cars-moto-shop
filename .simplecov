db08e24b151c89c6422ad68f3944cc89fef64a94
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-02-27 22:53:56 +0200

    Added circleci for codacy 4

--- .simplecov
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-SimpleCov.formatters = [SimpleCov::Formatter::JSONFormatter]
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 # SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::LcovFormatter,
+  SimpleCov::Formatter::JSONFormatter,
   SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start 'rails' do
