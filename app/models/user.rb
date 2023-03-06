e3580cdbbc06801e827d520a415a6900fa9e2d10
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-03-06 19:00:04 +0200

    Fixed bug whith validation, searches does not worked

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :searches, dependent: :delete_all
-
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, length: { within: 8..128 }
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  has_many :searches, dependent: :delete_all
 end
