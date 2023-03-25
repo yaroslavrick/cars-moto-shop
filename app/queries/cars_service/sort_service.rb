7000a921887f2c2849d7c674ebf13be72c9942ab
Yaroslav Yenkala <yaroslavrick@gmail.com>
2023-03-25 20:48:56 +0200

    Added interactor to SortService

--- app/queries/cars_service/sort_service.rb
+++ app/queries/cars_service/sort_service.rb
@@ -2,15 +2,23 @@
 
 module CarsService
   class SortService
+    # attr_reader :data
+    #
+    # def initialize(params:, data: Car.all)
+    #   @params = params
+    #   @data = data
+    # end
+    #
+    # def call
+    #   @data.filter_sort_by(@params)
+    # end
+    include Interactor
+
     attr_reader :data
 
-    def initialize(params:, data: Car.all)
-      @params = params
-      @data = data
-    end
-
     def call
-      @data.filter_sort_by(@params)
+      @data = Car.all
+      @data.filter_sort_by(context.params)
     end
   end
 end
