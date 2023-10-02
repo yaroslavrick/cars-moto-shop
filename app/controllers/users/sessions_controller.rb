e1d24a2a440b9f691bdc0697b21fd318674d69b0
Yaroslav <yaroslavrick@gmail.com>
2023-10-02 21:16:15 +0300

    2 methods more

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -19,13 +19,13 @@ module Users
     #   super
     # end
 
-    # def after_sign_out_path_for(_resource_or_scope)
-    #   new_user_session_path
-    # end
-    #
-    # def after_sign_in_path(resource_or_scope)
-    #   stored_location_for(resource_or_scope) || root_path
-    # end
+    def after_sign_out_path_for(_resource_or_scope)
+      new_user_session_path
+    end
+
+    def after_sign_in_path(resource_or_scope)
+      stored_location_for(resource_or_scope) || root_path
+    end
 
     # protected
 
